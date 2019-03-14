9b6d5c0873f9a4683e308097fe06a1f045a4abb5
Parth Shah <parth.shah@berkeley.edu>
2019-03-13 23:08:54 -0700

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <ul>
+      <li>
+      Home
+      </li>
+      <li>
+      About
+      </li>
+      <li>
+      Contact
+      </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navigation;
